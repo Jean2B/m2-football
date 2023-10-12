475487e473a3ad6e089b48dcea193d86cff11f88
Jean <90519788+Jean2B@users.noreply.github.com>
2023-10-12 15:54:02 +0200

    Initialisation Eureka

--- PlayerService/src/main/java/com/m2dfs/PlayerService/PlayerServiceApplication.java
+++ PlayerService/src/main/java/com/m2dfs/PlayerService/PlayerServiceApplication.java
@@ -2,8 +2,10 @@ package com.m2dfs.PlayerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlayerServiceApplication {
 
 	public static void main(String[] args) {
