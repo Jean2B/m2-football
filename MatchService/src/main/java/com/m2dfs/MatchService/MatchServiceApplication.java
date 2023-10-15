9b01ca035f3eedc6a9ce52c28d1761fb43170e4c
Jean <90519788+Jean2B@users.noreply.github.com>
2023-10-15 23:53:41 +0200

    Initialisation Ribbon

--- MatchService/src/main/java/com/m2dfs/MatchService/MatchServiceApplication.java
+++ MatchService/src/main/java/com/m2dfs/MatchService/MatchServiceApplication.java
@@ -1,11 +1,14 @@
 package com.m2dfs.MatchService;
 
+import com.m2dfs.MatchService.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RibbonClient(name = "server", configuration = RibbonConfig.class)
 public class MatchServiceApplication {
 
 	public static void main(String[] args) {
